002838bbf738e8516876a2c8648a47f688d61419
ali <ali.w01rk@gmail.com>
2026-09-12 16:11:33 +0330

    edit delete modal test in address page

--- cypress/e2e/general/address/delete.cy.ts
+++ cypress/e2e/general/address/delete.cy.ts
@@ -6,14 +6,16 @@ describe("Delete address modal tests", () => {
   });
 
   it("Delete a row from table", () => {
-    cy.get('div:has(data-rowindex="12")').click();
-
-    cy.get(
-      'button:has(path[d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"])',
-    ).click();
+    cy.get('[role="row"]')
+      .contains('[data-field="code"]', "test-value")
+      .click()
+      .get('span[aria-label="Delete"] button')
+      .click();
 
     cy.contains("Delete Item").should("be.visible");
 
     cy.get("button").contains("Delete").click();
+
+    cy.contains("test-value").should("not.be.visible");
   });
 });
